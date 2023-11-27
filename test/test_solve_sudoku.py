d2e33881b5939fa64f47a67d06bc66d7eb2a7777
Cailley Factor <cf593@cam.ac.uk>
2023-11-27 17:14:27 +0000

    added example puzzle to this test

---
+++ test/test_solve_sudoku.py
@@ -0,0 +1,37 @@
+from src.solve_sudoku import solve_sudoku
+import numpy as np
+
+
+def test_solve_sudoku():
+    """
+    Tests that it solves an example puzzle. Asked ChatGPT to give me an example puzzle with prompt:
+    "example sudoku puzzle and answer in python as numpy array"
+    """
+    puzzle = np.array(
+        [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9],
+        ]
+    )
+
+    puzzle_answer = np.array(
+        [
+            [5, 3, 4, 6, 7, 8, 9, 1, 2],
+            [6, 7, 2, 1, 9, 5, 3, 4, 8],
+            [1, 9, 8, 3, 4, 2, 5, 6, 7],
+            [8, 5, 9, 7, 6, 1, 4, 2, 3],
+            [4, 2, 6, 8, 5, 3, 7, 9, 1],
+            [7, 1, 3, 9, 2, 4, 8, 5, 6],
+            [9, 6, 1, 5, 3, 7, 2, 8, 4],
+            [2, 8, 7, 4, 1, 9, 6, 3, 5],
+            [3, 4, 5, 2, 8, 6, 1, 7, 9],
+        ]
+    )
+    assert (solve_sudoku(puzzle) == puzzle_answer).all()
