6ac4a2be3d1f4e247d5023fb9034395829e8d79f
Cailley Factor <cf593@cam.ac.uk>
2023-11-27 14:33:12 +0000

    adding another test for the exists_conflict function

---
+++ test/test_no_conflict.py
@@ -0,0 +1,26 @@
+from src.solve_sudoku import exists_conflict
+import numpy as np
+
+
+def test_no_conflict():
+    """
+    Tests whether there exists a conflict
+    """
+    empty_index = (0, 0)
+    puzzle = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 4, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 5, 0, 9, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    puzzle = np.array(puzzle)
+    assert (
+        not exists_conflict(puzzle, empty_index, 4)
+        or exists_conflict(puzzle, empty_index, 5)
+        or exists_conflict(puzzle, empty_index, 9)
+    )
