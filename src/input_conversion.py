3affa0efdc5ccff96745786f68b902ab59aa36eb
Cailley Factor <cf593@cam.ac.uk>
2023-12-08 14:16:34 +0000

    improved error traps using ValueError returns

--- src/input_conversion.py
+++ src/input_conversion.py
@@ -5,6 +5,7 @@ def input_converter(puzzle):
     # error trap if input is not a string
     if not isinstance(puzzle, str):
         print("Input must be a string.")
+        return 0
 
     # removing the symbols from the input.txt to form an int string
     puzzle = puzzle.replace("---+---+---", "")
@@ -16,23 +17,19 @@ def input_converter(puzzle):
     if len_before > len_after:
         print("Input reformatted: contained extra spaces")
 
-    # Ensure digits only
+    # Ensure digits only in th Sudoku grid
     if not puzzle.isdigit():
-        print(
-            "Unexpected characters: check delimiters and ensure that only digits are in the puzzle"
+        raise ValueError(
+            "Incorrect input value: incorrect delimiters or non-digit characters in the puzzle"
         )
 
     # error trap to make sure 81 digits/characters
     if len(puzzle) != 81:
-        print("Incorrect input value: too many characters")
+        raise ValueError("Incorrect input value: too many characters")
 
     # converting the integer string to a numpy array
     np_array = np.array([int(digit) for digit in str(puzzle)])
     puzzle = np_array.reshape((9, 9))
-
-    # error trap to make sure that all digits are between 0-9
-    if np.all(puzzle > 9) or np.all(puzzle < 0):
-        print("Incorrect input value: digits/characters not between 0-9")
 
     # error trap to make sure that there are no duplicates in a puzzle row
     for i in range(9):
@@ -42,7 +39,7 @@ def input_converter(puzzle):
         unique_numbers, counts = np.unique(puzzle_row, return_counts=True)
         duplicates = unique_numbers[counts > 1]
         if duplicates.size > 0:
-            print("Incorrect input value: duplicates in a row")
+            raise ValueError("incorrect input value: duplicates in a row")
 
     # error trap to make sure that there are no duplicates in a puzzle column
     for j in range(9):
@@ -52,5 +49,6 @@ def input_converter(puzzle):
         unique_numbers, counts = np.unique(puzzle_col, return_counts=True)
         duplicates = unique_numbers[counts > 1]
         if duplicates.size > 0:
-            print("Incorrect input value: duplicates in the columns")
+            raise ValueError("Incorrect input value: duplicates in the columns")
+
     return puzzle
